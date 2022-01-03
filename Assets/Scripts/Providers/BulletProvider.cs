1acc3bd78d86d253c64af25355a3c978bea03f9f
BreaCode <oolalkoo@gmail.com>
2022-01-03 13:56:30 +0300

    Providers

---
+++ Assets/Scripts/Providers/BulletProvider.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BulletProvider : MonoBehaviour
+{
+    void OnBecameInvisible()
+    {
+
+    }
+
+    void OnTriggerEnter()
+    {
+
+    }
+
+    void ReturnToPool()
+    {
+
+    }
+}
