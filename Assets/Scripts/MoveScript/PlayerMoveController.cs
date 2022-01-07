4ac14574b4374e5b5db23246239da62f3463ed08
AR1497 <88129242+AR1497@users.noreply.github.com>
2022-01-07 19:54:38 +0300

    Motion

---
+++ Assets/Scripts/MoveScript/PlayerMoveController.cs
@@ -0,0 +1,17 @@
+﻿namespace GeekSpace
+{
+    internal class PlayerMoveController : IExecute 
+    {
+        player2 _player;
+
+        internal PlayerMoveController(player2 player)
+        {
+            _player = player;
+        }
+
+        public void Execute(float delteTime)
+        {
+            _player.Move(delteTime);
+        }
+    }
+}
