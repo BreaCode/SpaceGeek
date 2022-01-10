04beb28a05d56e598297cc14ef4c3b3c24050d58
AR1497 <88129242+AR1497@users.noreply.github.com>
2022-01-11 00:18:34 +0300

    CorrectMove

--- Assets/Scripts/MoveScript/MoveTransform.cs
+++ Assets/Scripts/MoveScript/MoveTransform.cs
@@ -5,21 +5,36 @@ namespace GeekSpace
     internal class MoveTransform : IMoveble
     {
         private PlayerModel playerModel;
+        private IUserInputProxy _horizontalInputProxy;
+        private IUserInputProxy _verticalInputProxy;
+        private float horizontal;
+        private float vertical;
 
-        public MoveTransform(PlayerModel playerModel)
+        public MoveTransform(PlayerModel playerModel, (IUserInputProxy horizontalInputProxy, IUserInputProxy verticalInputProxy) GetInput)
         {
             this.playerModel = playerModel;
+            _horizontalInputProxy = GetInput.horizontalInputProxy;
+            _verticalInputProxy = GetInput.verticalInputProxy;
+            _horizontalInputProxy.AxisOnChange += HorizontalOnAxisOnChange;
+            _verticalInputProxy.AxisOnChange += VerticalOnAxisOnChange;
+        }
+
+        void VerticalOnAxisOnChange(float value)
+        {
+            vertical = value;
+        }
+
+        void HorizontalOnAxisOnChange(float value)
+        {
+            horizontal = value;
         }
 
         public void Move(Transform transform)
         {
-            float moveVertical = Input.GetAxis("Vertical");
-            float moveHorizontal = Input.GetAxis("Horizontal");
-
-            var _shipSpeed = Mathf.Lerp(playerModel.Speed, Mathf.Max(moveVertical, 0), 0.1f);
+            var _shipSpeed = Mathf.Lerp(playerModel.Speed, Mathf.Max(vertical, 0), 0.1f);
             transform.position += transform.up * _shipSpeed * playerModel.Speed * Time.deltaTime;
 
-            var rot = moveHorizontal;
+            var rot = horizontal;
             transform.rotation *= Quaternion.Euler(0, 0, rot);
         }
     }
