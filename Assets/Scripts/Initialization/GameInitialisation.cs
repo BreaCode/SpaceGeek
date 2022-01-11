b1f1c0136dad4b0dc904cf62aba988f24aac2346
BreaCode <oolalkoo@gmail.com>
2022-01-11 05:51:00 +0300

    Input&Move Fix

--- Assets/Scripts/Initialization/GameInitialisation.cs
+++ Assets/Scripts/Initialization/GameInitialisation.cs
@@ -14,8 +14,8 @@ namespace GeekSpace
             var input = new InputInitialization();
             var inputController = new InputController(input.GetInput());
 
-            var _playerModel = new PlayerModel("Prefabs/Ship/PlayerShip", WeaponType.ChainGunMk1, startPosition, 10, 1);
-            IMoveble _playerMove = new MoveTransform(_playerModel);
+            var _playerModel = new PlayerModel("Prefabs/Ship/PlayerShip", WeaponType.ChainGunMk1, startPosition, 10, 3);
+            IMoveble _playerMove = new MoveTransform(_playerModel, (input.GetInput().inputHorizontal, input.GetInput().inputVertical));
             var _player = new Player(_playerMove, _playerModel);
             var moveController = new PlayerMoveController(_player);
             var asteroidPrefab = (Resources.Load<EnemyProvider>(PathsManager.ASTEROID_PREFAB));
@@ -27,7 +27,7 @@ namespace GeekSpace
 
             var beyondScreenActer = new BeyondScreenActer();
 
-            _controllers.Add(input);
+            _controllers.Add(inputController);
             _controllers.Add(enemyController);
             _controllers.Add(moveController);
         }
