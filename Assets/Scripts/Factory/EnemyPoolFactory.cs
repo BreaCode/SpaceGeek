ea2c3ea7f832670819d1c62f4910ce5b4d635e66
BreaCode <oolalkoo@gmail.com>
2022-01-17 12:08:45 +0300

    Update EnemyPoolFactory.cs

--- Assets/Scripts/Factory/EnemyPoolFactory.cs
+++ Assets/Scripts/Factory/EnemyPoolFactory.cs
@@ -9,6 +9,16 @@ namespace GeekSpace
         internal static ObjectPool EnemyPoolCreate(EnemyType enemyType)
         {
             var enemyPrefab = (Resources.Load<EnemyProvider>(PathsManager.ASTEROID_PREFAB));
+            switch (enemyType)
+            {
+                case EnemyType.Asteroid:
+                    enemyPrefab = (Resources.Load<EnemyProvider>(PathsManager.ASTEROID_PREFAB));
+                    break;
+                case EnemyType.Ship:
+                    enemyPrefab = (Resources.Load<EnemyProvider>(PathsManager.ENEMY_SHIP_PREFAB));
+                    break;
+            }
+
 
             var poolRoot = new Vector3(0, 0, 0);
             var enemyPool = new ObjectPool(enemyPrefab.gameObject, poolRoot);
