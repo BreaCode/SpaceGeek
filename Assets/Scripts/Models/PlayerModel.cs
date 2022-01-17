cc54695b51cbfe1663eb37b030b596694d9b7937
BreaCode <oolalkoo@gmail.com>
2022-01-17 23:50:57 +0300

    Update PlayerModel.cs

--- Assets/Scripts/Models/PlayerModel.cs
+++ Assets/Scripts/Models/PlayerModel.cs
@@ -6,6 +6,7 @@ namespace GeekSpace
     {
         readonly private string _pathToPrefab;
         readonly private WeaponType _weaponType;
+        private WeaponModel _weaponModel;
         private Vector3 _position;
         private GameObject _playerObject;
         private IPool _pool;
@@ -41,10 +42,11 @@ namespace GeekSpace
             get { return _speed; }
         }
 
-        public PlayerModel(string pathToPrefab, WeaponType weaponType, Vector3 position, int healthPoitns, float speed)
+        public PlayerModel(string pathToPrefab, WeaponType weaponType, WeaponModel weaponModel, Vector3 position, int healthPoitns, float speed)
         {
             _pathToPrefab = pathToPrefab;
             _weaponType = weaponType;
+            _weaponModel = weaponModel;
             _position = position;
             _healthPoitns = healthPoitns;
             _speed = speed;
