690f17de62e36a2f6161a7bd0034c228beda5360
Maksim.S <maxsic85@mail.ru>
2022-01-22 12:31:26 +0300

    add const

--- Assets/SceneManagment.cs
+++ Assets/SceneManagment.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -9,51 +10,51 @@ namespace GeekSpace
     public class SceneManagment : MonoBehaviour
     {
 
-        public UnityEngine.Audio.AudioMixer _audioMixer;
-        Resolution[] resolutionsList;
-        List<string> resolutions;
-        public Dropdown ResolutionDropDown;
-        public Dropdown QualityDropDown;
-        public Slider sliderAudio;
+        public AudioMixer _audioMixer;
+        Resolution[] _resolutionsArray;
+        List<string> _resolutionsStringList;
+        public Dropdown _resolutionDropDown;
+        public Dropdown _qualityDropDown;
+        public Slider _sliderAudio;
         public Animator _animator;
         public void Awake()
         {
-            resolutions = new List<string>();
-            resolutionsList = Screen.resolutions;
-            foreach (var i in resolutionsList)
+            _resolutionsStringList = new List<string>();
+            _resolutionsArray = Screen.resolutions;
+            foreach (var i in _resolutionsArray)
             {
-                resolutions.Add(i.width + "x" + i.height);
+                _resolutionsStringList.Add(i.width + "x" + i.height);
             }
-            ResolutionDropDown.ClearOptions();
-            ResolutionDropDown.AddOptions(resolutions);
+            _resolutionDropDown.ClearOptions();
+            _resolutionDropDown.AddOptions(_resolutionsStringList);
         }
 
         public void ShowMenu()
         {
-            var menuOpen = _animator.GetBool("IsOpenMenu");
-            if (menuOpen) _animator.SetBool("IsOpenMenu", false);
-            else _animator.SetBool("IsOpenMenu", true);
+            var menuOpen = _animator.GetBool(SceeneConstManager.SETTING_MENU_TRIGGER);
+            if (menuOpen) _animator.SetBool(SceeneConstManager.SETTING_MENU_TRIGGER, false);
+            else _animator.SetBool(SceeneConstManager.SETTING_MENU_TRIGGER, true);
             _animator.gameObject.transform.SetAsLastSibling();
         }
 
         public void AudioVolume()
         {
-            _audioMixer.SetFloat("Volume", sliderAudio.value);
+            _audioMixer.SetFloat(SceeneConstManager.AUDIO_MIXER_VOLUME_LABEL, _sliderAudio.value);
         }
 
         public void Resolution()
         {
 
-            Screen.SetResolution(resolutionsList[ResolutionDropDown.value].width, resolutionsList[ResolutionDropDown.value].height, true);
+            Screen.SetResolution(_resolutionsArray[_resolutionDropDown.value].width, _resolutionsArray[_resolutionDropDown.value].height, true);
         }
         public void Quality()
         {
-            QualitySettings.SetQualityLevel(QualityDropDown.value, true);
+            QualitySettings.SetQualityLevel(_qualityDropDown.value, true);
         }
 
         public void PlayPressed()
         {
-            SceneManager.LoadScene("SampleScene");
+            SceneManager.LoadScene(SceeneConstManager.MAIN_SCENE_NAME);
         }
 
         public void ExitPressed()
