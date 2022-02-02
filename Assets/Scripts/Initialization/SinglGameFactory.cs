075225de77e17db74e0af26eecde698db4a67587
Maksim.S <maxsic85@mail.ru>
2022-02-02 16:01:22 +0300

    add SingleGameFactory

---
+++ Assets/Scripts/Initialization/SinglGameFactory.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+namespace GeekSpace
+{
+    internal class SinglGameFactory : IAbstractGameFactory
+    {
+        Camera camera = Camera.main;
+        System.Random random = new System.Random();
+        private IPool gunBulletPool;
+        private Player player;
+        private Controllers _controllers;
+
+        public SinglGameFactory(Controllers controllers)
+        {
+            _controllers = controllers;
+
+        }
+
+        public void CreateEnemy()
+        {
+            var enemyPoolAsteroid = EnemyPoolFactory.EnemyPoolCreate(EnemyType.Asteroid);
+            var enemyAsteroidPoolOperator = new EnemyPoolOperator(enemyPoolAsteroid, MaximumsManager.ASTEROIDS_MAXIMUM, EnemyType.Asteroid);
+            var timerSystemAsteroidSpawn = new TimerSystem(true, true, 15);
+            IMoveble nullMove = new MoveNOTHING();
+            var enemyAsteroidController = new EnemyController(timerSystemAsteroidSpawn, enemyPoolAsteroid, random, nullMove);
+            var enemyPoolShip = EnemyPoolFactory.EnemyPoolCreate(EnemyType.Ship);
+            var enemyShipPoolOperator = new EnemyPoolOperator(enemyPoolShip, MaximumsManager.SHIP_MAXIMUM, EnemyType.Ship);
+            var timerSystemShipSpawn = new TimerSystem(true, true, 35);
+            var timerSystemShipShooting = new TimerSystem(true, true, enemyShipPoolOperator.CurrentModel.WeaponModel.Cooldown);
+            IMoveble shipMove = new MoveTransformEnemy(enemyShipPoolOperator.CurrentModel, player.PlayerProvider.gameObject);
+            var enemyShipController = new EnemyController(timerSystemShipSpawn, enemyPoolShip, random, shipMove);
+            IShootController enemyShootController = new EnemyShootController(timerSystemShipShooting, gunBulletPool, enemyShipPoolOperator.CurrentModel.Object.transform, enemyShipPoolOperator.CurrentModel.Object, EnemyParametrsManager.TARGET_LAYER);
+            _controllers.Add(enemyAsteroidController);
+            _controllers.Add(enemyShipController);
+            _controllers.Add(enemyShootController);
+        }
+
+        public void CreatePlayer()
+        {
+            var startPosition = camera.GetCentrAccordingCamera();
+            var input = new InputInitialization();
+            var inputController = new InputController(input.GetInput());
+
+            var playerWeaponModel = WeaponModelFactory.WeaponModelCreate(WeaponType.ChainGunMk1);
+            var playerModel = new PlayerModel(PathsManager.PLAYER_PREFAB, WeaponType.ChainGunMk1, playerWeaponModel, startPosition, PlayerParametrsManager.PLAYER_HEALTH, PlayerParametrsManager.PLAYER_SPEED);
+            IMoveble playerMove = new MoveTransform(playerModel, (input.GetInput().inputHorizontal, input.GetInput().inputVertical));
+
+            player = new Player(playerMove, playerModel);
+            var playerMoveController = new PlayerMoveController(player);
+            gunBulletPool = BulletPoolFactory.BulletPoolCreate(WeaponType.ChainGunMk1);
+            var bulletModel = new BulletModel(gunBulletPool, player.PlayerProvider.transform.position, 2);
+            var bulletPoolOperator = new BulletPoolOperator(gunBulletPool, bulletModel, MaximumsManager.BULLETS_MAXIMUM);
+            var playerReloadCooldown = player.PlayerProvider.PlayerModel.WeaponModel.Cooldown;
+            var shootTimer = new TimerSystem(true, true, playerReloadCooldown);
+            IShootController playerShootController = new ShootControllerWithAutoShoot(shootTimer, gunBulletPool, player.PlayerProvider.transform, player.PlayerProvider.gameObject, PlayerParametrsManager.TARGET_LAYER);
+
+            _controllers.Add(inputController);
+            _controllers.Add(playerMoveController);
+            _controllers.Add(playerShootController);
+        }
+
+        public InputInitialization SetInput()
+        {
+            var input = new InputInitialization();
+            return input;
+        }
+
+    }
+}
