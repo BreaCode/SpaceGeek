9c2688b12c8b429ac843b55bde5cf1b19fb3f3b8
BreaCode <oolalkoo@gmail.com>
2022-02-08 08:35:10 +0300

    OnGUI create

---
+++ Assets/Scripts/OnGUI/CheatGUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GeekSpace
+{
+    public class CheatGUI : MonoBehaviour
+    {
+        void OnGUI()
+        {
+        
+        }
+
+    }
+}
